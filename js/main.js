09055cbbb7cebbba441a1fa0eb78d53b07bf71ff
Adam Pearson <adam.pearson94@gmail.com>
2021-06-17 15:10:21 +0100

    Implemented side menu
    - Opens on burger menu click
    - Closes on close click, or on overlay click

--- js/main.js
+++ js/main.js
@@ -4,10 +4,50 @@ var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
     loop: false,
-    cursor: ""
+    cursor: "",
+    delay: 50
 });
 
 typewriter.typeString('<h1>My Name is Adam Pearson</h1>')
-    .pauseFor(1250)
+    .pauseFor(750)
     .typeString('<h2 class="strapline">I\'m a Web Developer')
     .start();
+
+// Side bar
+
+var burger = $('#burger');
+var sidemenu = $('#sidebar');
+var close = $('.close');
+var overlay = $('.overlay');
+
+
+burger.on('click', function() {
+    sidemenu.show();
+    sidemenu.animate({'left': 0}, 300);
+    sidemenu.addClass('shown');
+    overlay.show();
+    overlay.animate({'opacity': 1}, 150);
+    overlay.addClass('shown');
+});
+
+close.on('click', function() {
+    sidemenu.hide();
+    sidemenu.removeClass('shown');
+    overlay.hide();
+    overlay.removeClass('shown');
+});
+
+overlay.on('click', function() {
+    sidemenu.hide();
+    sidemenu.removeClass('shown');
+    overlay.hide();
+    overlay.removeClass('shown');
+});
+
+$(window).resize(function() {
+    if ($(window).width() >= 768) {
+        sidemenu.removeClass('shown');
+        overlay.hide();
+        overlay.removeClass('shown');
+    }
+});
