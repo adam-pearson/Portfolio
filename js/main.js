3119d6355601e3288d94ee21f1dad02597d67a05
Adam Pearson <adam.pearson94@gmail.com>
2021-06-18 14:46:17 +0100

    Added form functionality (for whenever I'm able to send it?)

--- js/main.js
+++ js/main.js
@@ -101,3 +101,65 @@ $('.nav a[href*="#contact"]').on('click', function() {
     window.addEventListener('scroll', checkScroll);
   
 })();
+
+//contact form
+
+// var fields = {};
+
+// $.ready(function() {
+//     fields.firstName = $('#first-name');
+//     fields.lastName = $('#last-name');
+//     fields.email = $('#email');
+//     fields.subject = $('#subject');
+//     fields.message = $('#message');
+// });
+
+// function isNotEmpty(value) {
+//     if (value == null || typeof value == "undefined") return false;
+//     return (value.length > 0);
+// }
+
+// function isEmail(email) {
+//     var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+//     return regex.test(String(email).toLowerCase());
+//    }
+
+// function fieldValidation(field, validationFunction) {
+//     if (field == null) return false;
+
+//     var isFieldValid = validationFunction(field.value);
+//     if (!isFieldValid) {
+//     field.addClass('placeholderRed');
+//     } else {
+//     field.removeClass('placeholderRed');
+//     }
+
+//     return isFieldValid;
+// }
+
+// function isValid() {
+//     var valid = true;
+//     valid &= fieldValidation(fields.firstName, isNotEmpty);
+//     valid &= fieldValidation(fields.lastName, isNotEmpty);
+//     valid &= fieldValidation(fields.email, isNotEmpty);
+//     valid &= fieldValidation(fields.email, isEmail);
+//     valid &= fieldValidation(fields.subject, isNotEmpty);
+//     valid &= fieldValidation(fields.message, isNotEmpty);
+//     return valid;
+// }
+
+// class User {
+//     constructor(firstName, lastName, email, subject, message) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.email = email;
+//         this.subject = subject;
+//         this.message = message;
+//     }
+// }
+
+// function sendContact() {
+//     if (isValid()) {
+//         var userContact = new User(firstName.value, lastName.value, email.value, subject.value, message.value);
+//     }
+// }
